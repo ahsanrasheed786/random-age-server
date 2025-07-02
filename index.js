20c2ed652bafeedb66b7012d298bb5f62a4e48b5
ahsanrasheed786 <69690550+ahsanrasheed786@users.noreply.github.com>
2025-07-02 14:08:38 +0500

    cleaning code

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-// index.js
+// // index.js
 
+// const express = require('express');
+// const cors = require('cors');  // Import the CORS package
+
+// const app = express();
+
+// // Use CORS middleware to allow all origins (or you can specify an origin)
+// app.use(cors());  // This will allow CORS for all routes
+
+// const random = require('./api/random');
+
+// module.exports = random;
+
+
+// index.js
 const express = require('express');
 const cors = require('cors');  // Import the CORS package
+const random = require('./api/random');
 
 const app = express();
 
-// Use CORS middleware to allow all origins (or you can specify an origin)
-app.use(cors());  // This will allow CORS for all routes
+// Use CORS middleware to allow all origins
+app.use(cors());  // Enables CORS for all routes
 
-const random = require('./api/random');
+// Attach the random API route
+app.use(random);  // This will route to the /api/random endpoint
 
-module.exports = random;
+// If you're running a server locally, listen on a port (e.g., 3000)
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
