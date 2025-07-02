9794fa0220e37c31e6b0de91904d06dc9121b860
ahsanrasheed786 <69690550+ahsanrasheed786@users.noreply.github.com>
2025-07-02 14:01:28 +0500

    cleaning code

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // index.js
 
+const express = require('express');
+const cors = require('cors');  // Import the CORS package
+// const random = require('./api/random');
+
+const app = express();
+
+// Use CORS middleware to allow all origins (or you can specify an origin)
+app.use(cors());  // This will allow CORS for all routes
+
 const random = require('./api/random');
 
 module.exports = random;
